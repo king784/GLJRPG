03f034561a24e7bfada810d9b39dbfc11d91a470
Teemu Turku <teemu.turku@pp2.inet.fi>
2019-09-01 10:32:04 +0300

    path for image

--- Game/main.cpp
+++ Game/main.cpp
@@ -141,7 +141,7 @@ int main()
 
     // Image loading
     int width, height, nrChannels;
-    unsigned char* data = stbi_load("C:/Users/teemu.turku/Documents/GitHub/GLJRPG/Game/Images/Background.png", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load((pathToRoot + "/Game/Images/Background.png").c_str(), &width, &height, &nrChannels, 0);
 
     if(data)
     {
