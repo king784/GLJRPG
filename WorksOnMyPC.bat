d644b9c4e149f95570b4bf5cd4cc4d364e1b6437
king784 <teemu.turku@pp2.inet.fi>
2019-09-06 12:38:39 +0300

    ye

--- WorksOnMyPC.bat
+++ WorksOnMyPC.bat
@@ -1,5 +1,5 @@
 @echo Building GLFW Application...
-g++ -I/Game Game/main.cpp Game/glad.c Game/Shader.cpp Game/Model.cpp Game/Camera.cpp Game/Texture.cpp Game/AudioManager.cpp -static-libstdc++ -static-libgcc -lglfw3 -lopengl32 -lgdi32 -g -o Game.exe
+g++ -I/Game Game/main.cpp Game/glad.c Game/Shader.cpp Game/Model.cpp Game/Camera.cpp Game/Texture.cpp Game/AudioManager.cpp -static-libstdc++ -static-libgcc -lglfw3 -lopengl32 -lgdi32 -g -o Build/Game.exe
 @echo Complete, running
 pause
 Game.exe
