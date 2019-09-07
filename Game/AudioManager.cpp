222cd6387753807c57133254d988ff4cc7aa99e5
Teemu Turku <teemu.turku@pp2.inet.fi>
2019-09-07 19:28:31 +0300

    yw

--- Game/AudioManager.cpp
+++ Game/AudioManager.cpp
@@ -21,7 +21,57 @@ Audiomanager::Audiomanager()
     {
         // Enumeration is supported.
         GetAudioDevices(alcGetString(0, ALC_DEVICE_SPECIFIER));
-    }   
+    }
+
+    // Create and initialize a context.  
+    context = alcCreateContext(audioDevice, 0);
+    if(!alcMakeContextCurrent(context))
+    {
+        std::cerr << "Failed to make audio context!" << std::endl;
+    } 
+
+    CheckForErrors();
+
+    // Set listener
+    ALfloat newListenPoint[] = {0.0f, 0.0f, 0.0f};
+    SetListenerPoint(newListenPoint);
+
+    // Create source
+    CreateSource();
+    
+    bgMusic.data = LoadWav("D:/Projects/OpenGL/GLJRPG/Game/Sounds/Music/alenarag.wav", 
+    bgMusic.channel, bgMusic.sampleRate, bgMusic.bps, bgMusic.size);
+
+    SetAudioFormat(bgMusic);
+
+    // Generate buffer and buffer the data
+    alGenBuffers((ALuint)1, &buffer);
+
+    alBufferData(buffer, bgMusic.format, bgMusic.data, bgMusic.size, bgMusic.sampleRate);
+
+    CheckForErrors();
+
+    // Play the source
+    alSourcePlay(source);
+    CheckForErrors();
+
+    // Get source state
+    ALint source_state;
+    alGetSourcei(source, AL_SOURCE_STATE, &source_state);
+    while(source_state == AL_PLAYING)
+    {
+        alGetSourcei(source, AL_SOURCE_STATE, &source_state);
+        CheckForErrors();
+    }
+}
+
+Audiomanager::~Audiomanager()
+{
+    alDeleteSources(1, &source);
+    alDeleteBuffers(1, &buffer);
+    alcDestroyContext(context);
+    alcCloseDevice(audioDevice);
+    delete[] bgMusic.data;
 }
 
 void Audiomanager::GetAudioDevices(const ALCchar* devices)
@@ -39,4 +89,97 @@ void Audiomanager::GetAudioDevices(const ALCchar* devices)
         next += (len + 2);
     }
     std::cout << "------------" << std::endl;
+}
+
+void Audiomanager::SetListenerPoint(const ALfloat* newPoint)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        listenerPoint[i] = newPoint[i];
+    }
+    alListener3f(AL_POSITION, 0, 0, 1.0f);
+    CheckForErrors();
+}
+
+void Audiomanager::CreateSource()
+{
+    alGenSources((ALuint)1, &source);
+    CheckForErrors();
+    alSourcef(source, AL_PITCH, 1);
+    CheckForErrors();
+    alSourcef(source, AL_GAIN, 1);
+    CheckForErrors();
+    alSource3f(source, AL_POSITION, 0, 0, 0);
+    CheckForErrors();
+    alSource3f(source, AL_VELOCITY, 0, 0, 0);
+    CheckForErrors();
+    alSourcei(source, AL_LOOPING, AL_FALSE);
+    CheckForErrors();
+}
+
+char* Audiomanager::LoadWav(const char* fn, int& chan, int& samplerate, int& bps, int& size)
+{
+    char buffer[4];
+    std::ifstream in(fn, std::ios::binary);
+    in.read(buffer, 4);
+    if (strncmp(buffer, "RIFF", 4) != 0)
+    {
+        std::cout << "this is not a valid WAVE file" << std::endl;
+        return NULL;
+    }
+    in.read(buffer, 4);
+    in.read(buffer, 4);      //WAVE
+    in.read(buffer, 4);      //fmt
+    in.read(buffer, 4);      //16
+    in.read(buffer, 2);      //1
+    in.read(buffer, 2);
+    chan = ConvertToInt(buffer, 2);
+    in.read(buffer, 4);
+    samplerate = ConvertToInt(buffer, 4);
+    in.read(buffer, 4);
+    in.read(buffer, 2);
+    in.read(buffer, 2);
+    bps = ConvertToInt(buffer, 2);
+    in.read(buffer, 4);      //data
+    in.read(buffer, 4);
+    size = ConvertToInt(buffer, 4);
+    char* data = new char[size];
+    in.read(data, size);
+    return data;
+}
+
+void Audiomanager::SetAudioFormat(Sound& sound)
+{
+    if (sound.channel == 1)
+    {
+        if (sound.bps == 8)
+        {
+            sound.format = AL_FORMAT_MONO8;
+        }
+        else 
+        {
+            sound.format = AL_FORMAT_MONO16;
+        }
+    }
+    else {
+        if (sound.bps == 8)
+        {
+            sound.format = AL_FORMAT_STEREO8;
+        }
+        else 
+        {
+            sound.format = AL_FORMAT_STEREO16;
+        }
+    }
+}
+
+void Audiomanager::CheckForErrors()
+{
+    // Check error
+    ALCenum error;
+    error = alGetError();
+    if (error != AL_NO_ERROR)
+    {
+        std::cerr << "Error! Here is what happened: " << error << std::endl;
+    }
 }
--- Game/AudioManager.h
+++ Game/AudioManager.h
@@ -2,19 +2,45 @@
 #define AUDIOMANAGER_H
 
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <string>
 
-#include <irrKlang.h>
-using namespace irrklang;
+#include <AL/al.h>
+#include <AL/alc.h>
+
+#include "CustomMath.h"
+
+struct Sound
+{
+    int channel;
+    int sampleRate;
+    int bps;
+    int size;
+    int format;
+    char* pathToSound;
+    char* data;
+};
 
 class Audiomanager
 {
 public:
     Audiomanager();
+    ~Audiomanager();
+    void GetAudioDevices(const ALCchar* devices);
+    void SetListenerPoint(const ALfloat* newPoint);
+    void CreateSource();
+    char* LoadWav(const char* fn, int& chan, int& samplerate, int& bps, int& size);
+    void SetAudioFormat(Sound& sound);
+    void CheckForErrors();
 
 private:
-    ISoundEngine* soundEngine;
+    ALCdevice* audioDevice;
+    ALCcontext* context;
+    ALfloat listenerPoint[3];
+    ALuint source;
+    ALuint buffer;
+    Sound bgMusic;
 };
 
 #endif
