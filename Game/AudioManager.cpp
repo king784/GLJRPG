50e448b3955218424b23a69dcd0dccd3e2b0cb1b
king784 <teemu.turku@pp2.inet.fi>
2019-10-17 16:06:53 +0300

    jee

--- Game/AudioManager.cpp
+++ Game/AudioManager.cpp
@@ -48,7 +48,7 @@ void Audiomanager::StartAudioManager()
     // Create source
     CreateSource();
     
-    char path[] = "D:/Projects/OpenGL/GLJRPG/Sounds/Music/alenarag.wav";
+    char path[] = "C:/Users/teemu.turku/Documents/GitHub/GLJRPG/Sounds/Music/alenarag.wav";
     // Load wav data
     alutLoadWAVFile((ALbyte*)path, &bgMusic.format, &bgMusic.data, &bgMusic.size, &bgMusic.frequency, &bgMusic.loop);
     // buffer = alutCreateBufferFromFile(path);
